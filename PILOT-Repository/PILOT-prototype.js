15fd24f28b3ea2a5bd99c42809b756ef828f8ab4
tflad <37784534+tflad@users.noreply.github.com>
2018-03-25 20:21:24 -0400

    Add files via upload

---
+++ PILOT-Repository/PILOT-prototype.js
@@ -0,0 +1,27 @@
+var myProgram = function()
+{
+	
+	<div id="myScreen" style="position:absolute;top:40px;left:40px;right:0px;bottom:0px;"></div>
+	
+	var screen = document.getElementById('myScreen');
+	screen.style.backgroundColor='grey';
+	screen.style.position='absolute';
+    
+	
+/*
+	var one = document.getElementById('one');
+	one.style.backgroundColor='#4a4a4a';  
+	one.src = "PILOT-Gesture-Chart.png";
+	
+	var two = document.getElementById('two');
+	two.style.backgroundColor='#4a4a4a';  
+	
+	var myDIV = document.getElementById('myDIV');
+	myDIV.style.backgroundColor='#fff200';
+	myDIV.style.width="100%";
+	myDIV.style.height="100%";
+	myDIV.style.position="absolute";
+	*/
+	
+
+}
